d420632a8372aab37388f226a95395dc04013e00
moealf12 <39033136+moealf12@users.noreply.github.com>
2019-12-21 19:43:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express=require('express')
 const app=express()
 const cors=require('cors')
 const mongoose=require('mongoose')
+var path = require('path');
 PORT = 4000 || process.env.PORT
 
 
